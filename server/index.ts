600a1ff065b2003483a54e1040469a6b3bef7e91
Roy-Ghandour <ghandourroy@gmail.com>
2026-01-10 21:21:48 +0400

    Staging change test

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono';
 const app = new Hono();
 
 app.get('/', c => {
-  return c.text('Hello Hono! This is a test deployment.');
+  return c.text('Hello Hono! This is a test deployment. THis is a change on staging');
 });
 
 export default app;
